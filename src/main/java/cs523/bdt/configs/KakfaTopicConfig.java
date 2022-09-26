25093bc681c3550014e1220e8780e5e01d92db09
unsmoker <hasangaly@gmail.com>
2022-09-25 19:00:02 -0500

    additional informations

--- src/main/java/cs523/bdt/configs/KakfaTopicConfig.java
+++ src/main/java/cs523/bdt/configs/KakfaTopicConfig.java
@@ -1,18 +1,14 @@
 package cs523.bdt.configs;
 
-import com.launchdarkly.eventsource.EventHandler;
-import com.launchdarkly.eventsource.MessageEvent;
+
 import org.apache.kafka.clients.admin.NewTopic;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
+
 
 @Configuration
 @PropertySource("classpath:kafka.properties")
